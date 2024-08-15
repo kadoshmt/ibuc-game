3599bd3504571216d5dd94ce011b1068bf6cff68
Janes Roberto <kadoshmt2@gmail.com>
2024-08-15 13:54:55 -0400

    feature: add PWA support

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "IBUC QUIZ",
+  title: "Aventuras pela Bíblia",
   description: "Aprendendo a Bíblia Sagrada através de perguntas e respostas.",
 };
 
@@ -17,6 +17,10 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+      <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui, orientation=landscape"/>
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <link rel="icon" href="/favicon.png" />
       </head>
       <body className={inter.className}>{children}</body>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
+const nextConfig = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development', // Desativa o PWA no ambiente de desenvolvimento
+    register: true,
+    skipWaiting: true
+  }
+});
 
 export default nextConfig;
