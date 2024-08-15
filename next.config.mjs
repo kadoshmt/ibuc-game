cc66ca2b37e44d6d0003ddb1d5efcf4477dfda50
Janes Roberto <kadoshmt2@gmail.com>
2024-08-15 14:04:54 -0400

    fix: remove PWA temporaly

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
-import withPWA from 'next-pwa';
-const nextConfig = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development', // Desativa o PWA no ambiente de desenvolvimento
-    register: true,
-    skipWaiting: true
-  }
-});
+
+const nextConfig = {};
 
 export default nextConfig;
