f6231a113b9376554a7fffa828e32f615597baf0
Janes Roberto <kadoshmt2@gmail.com>
2024-08-19 14:27:07 -0400

    fix: refactor components

---
+++ components/IconButton/index.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { useState } from 'react';
+import Image from 'next/image';
+
+interface IconButtonProps {
+  name: string; // Nome do botão
+  size?: number; // Tamanho (largura e altura) do botão
+  float?: 'none' | 'left' | 'right';
+  floatRight?: boolean;
+  onClick: () => void; // Método a ser chamado ao clicar no botão
+}
+
+const IconButton: React.FC<IconButtonProps> = ({ name, size = 84, float = 'none', onClick }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
+
+  return (
+    <>       
+      {(float === 'left' || float === 'right') && (
+        <div className={`fixed bottom-2 ${float}-8 flex items-center justify-center`}>
+          <div
+            onClick={onClick}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            style={{ width: size, height: size, cursor: 'pointer' }}
+          >
+            <Image
+              src={isHovered ? `/btn-${name}-hover.png` : `/btn-${name}.png`}
+              alt={name}
+              width={size}
+              height={size-1}
+            />
+          </div>
+        </div>
+      )}
+
+      {float === 'none' && (
+        <div
+          onClick={onClick}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          style={{ width: size, height: size, cursor: 'pointer' }}
+        >
+          <Image
+            src={isHovered ? `/btn-${name}-hover.png` : `/btn-${name}.png`}
+            alt={name}
+            width={size}
+            height={size-1}
+          />
+        </div>
+       )}
+    </>
+  );
+};
+
+export default IconButton;
--- components/InfoModal/index.tsx
+++ components/InfoModal/index.tsx
@@ -2,8 +2,8 @@
 'use client';
 
 import Image from 'next/image';
-import { useState } from 'react';
-import Button from './Button';
+import Button from '@/components/Button';
+import './styles.css'; // Importe o arquivo CSS
 
 interface InfoModalProps {
   isOpen: boolean;
@@ -14,29 +14,25 @@ const InfoModal: React.FC<InfoModalProps> = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
-      <div className="p-6 max-w-xl mx-auto text-center" style={{ backgroundImage: `url(/board-wooden.png)`, backgroundSize: `cover` }}>
+    <div className="info-modal-overlay">
+      <div className="info-modal-content">
         <div className="m-4">
           <Image
-            src="/logo-ibuc.png" // Substitua com o caminho da imagem desejada
+            src="/logo-ibuc.png"
             alt="IBUC Logo"
             width={118}
             height={70}
-            className="mx-auto"
+            className="info-modal-logo"
           />
         </div>
-        <p className="text-white mb-4 px-4">
+        <p className="info-modal-text">
           Este jogo foi criado para auxiliar os alunos e professores do Curso de Teologia Infantil do IBUC (Instituto Bíblico Único Caminho). 
           Você pode utilizá-lo como uma ferramenta para reforçar o aprendizado em sala de aula, ou recomendar aos alunos para que estes possam testar o seus conhecimentos 
           sobre o conteúdo aprendido em sala de aula. Este jogo utiliza som de fundo, e pode ser desativado nos botões no canto inferior, se assim preferir.
-        </p>
-        {/*<button
-          onClick={onClose}
-          className="bg-orange-500 text-white px-4 py-2 rounded-lg"
-        >
-          Entendi!
-        </button>*/} 
-        <div className="flex justify-center items-center m-5"><Button label={'Entendi!'} onClick={onClose} width={140 } height={41} fontSize='1rem'/></div>
+        </p>      
+        <div className="flex justify-center items-center m-5">
+          <Button label={'Entendi!'} onClick={onClose} width={140} height={41} fontSize='1rem' />
+        </div>
       </div>
     </div>
   );
