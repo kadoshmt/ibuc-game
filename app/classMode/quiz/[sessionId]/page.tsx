93e80a3a0c92a2cf53fcd503148784cd9ea39981
Janes Roberto <kadoshmt2@gmail.com>
2024-08-21 14:46:39 -0400

    feet: make quizPage responsive

--- app/classMode/quiz/[sessionId]/page.tsx
+++ app/classMode/quiz/[sessionId]/page.tsx
@@ -10,6 +10,8 @@ import { playCorrectSound, playIncorrectSound } from '@/utils/soundEffects';
 import FullScreenLoader from '@/components/FullScreenLoader';
 import useAudio from '@/hooks/useAudio';
 import IconButton from '@/components/IconButton';
+import '@/app/styles/quizPage.css';
+import RotatePhone from '@/components/RotatePhone';
 
 const rammetto = Rammetto_One({ subsets: ["latin"], weight: "400" });
 const patrick = Patrick_Hand({ subsets: ["latin"], weight: "400" });
@@ -92,8 +94,8 @@ export default function Quiz() {
   }, [loading, startTime]);
 
   useEffect(() => {
-    const name = localStorage.getItem('playerName') || '';
-    const gender = localStorage.getItem('playerGender') || 'menino'; // Default to 'menino' if not set
+    const name = localStorage.getItem('playerName') || 'Visitante'; // Default  'Visitante' se não selecionado
+    const gender = localStorage.getItem('playerGender') || 'menino'; // Default  'menino' se não selecionado
     setPlayerName(name);
     setPlayerGender(gender);
   }, []);
@@ -187,76 +189,109 @@ export default function Quiz() {
   const { src: avatarSrc, bgColor: avatarBgColor } = getAvatarDetails();
 
   return (
-    <div className="flex items-center justify-center h-screen bg-cover bg-center" style={{ backgroundImage: 'url(/quiz-classmode.webp)' }}>
-      <div className="relative w-[1000px] h-[849px] flex flex-col justify-between" style={{ backgroundImage: 'url(/pergaminho.png)', backgroundSize: 'cover' }}>
-        <div className="text-center  w-full flex justify-center px-56 pt-14">
-          <div className={`flex items-center gap-2`}>
-            <div className={`rounded-full shadow-lg ${avatarBgColor} border-8 border-orange-700`}>
-              <Image src={avatarSrc} alt="Avatar" width={88} height={88} className="rounded-full" />
-            </div>
-            <span className={`${rammetto.className} bg-orange-700 text-white text-2xl p-2 px-6 rounded-full`}>{playerName}</span>
-          </div>
-        </div>
-        <div className={`${patrick.className} text-center mx-60  h-[460px] flex flex-col justify-between`}>
-          <div>
-            <p className="text-3xl text-amber-700 font-bold">{currentQuestion.question}</p>
-            <ul className="mt-4 text-2xl text-amber-700">
-              {currentQuestion.answers.map((option, index) => (
-                <li key={index}>
-                  <button
-                    onClick={() => handleAnswerSelect(index)}
-                    className={`w-full px-14 p-2 rounded-lg font-semibold text-left mb-2 ${!showFeedback ? 'hover:bg-amber-600 hover:text-amber-600 hover:bg-opacity-15' : ''} ${
-                      selectedAnswer === index
-                        ? option.isCorrect
-                          ? 'bg-green-800 text-green-800 bg-opacity-25'
-                          : 'bg-red-600 text-red-800 bg-opacity-25'
-                        : ''
-                    } ${flashIndex === index ? 'flash' : ''}`}
-                    disabled={showFeedback}
-                  >
-                    <span className="text-center bg-amber-300 bg-opacity-25 rounded  px-1 mr-1">{answerLabels[index]}</span> {option.text}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </div>
-          {showFeedback && !questions[questionIndex].answers[selectedAnswer!].isCorrect && (
-            <div className="text-lg text-center text-red-600 bg-amber-500 bg-opacity-10 rounded-lg p-1 mb-4 w-full">
-              <p>{explanation}</p>
-            </div>
-          )}
-          {showFeedback && (
-            <div className="text-center">
-              <button
-                onClick={handleNextQuestion}
-                className={`${rammetto.className} bg-[#bf360c] border-2 border-orange-800 text-white text-sm py-4 px-4 rounded-lg w-fit`}
-              >
-                {questionIndex + 1 >= questions.length ? 'Finalizar Quiz' : 'Próxima Pergunta'}
-              </button>
-            </div>
-          )}
+    <>
+      <RotatePhone />
+      <div className="quiz-container">
+        <div className="quiz-content">          
+          <div className='quiz-scroll-background'>
+            <div className='quiz-inner'>
 
-          {isPlaying !== null && (
-          <div className="fixed bottom-8 left-8 flex items-center justify-center">
-            {isPlaying ? <IconButton name="music-on" size={72} onClick={toggle} /> : <IconButton name="music-off" size={72} onClick={toggle} />}
-          </div>
-        )}
+              <div className="quiz-avatar-name">
+                <div className={`flex items-center gap-2`}>
+                  <div className={`rounded-full shadow-lg ${avatarBgColor} border-8 border-orange-700`}>
+                    <Image src={avatarSrc} alt="Avatar" width={88} height={88} className="rounded-full" />
+                  </div>
+                  <span className={`quiz-player-name ${rammetto.className}`}>{playerName}</span>
+                </div>
+              </div>
+              <div className={`quiz-question-container ${patrick.className}`}>
+                <div>
+                  <p className="quiz-question">{currentQuestion.question}</p>
+                  <ul className="quiz-answer-list">
+                    {currentQuestion.answers.map((option, index) => (
+                      <li key={index}>
+                        <button
+                          onClick={() => handleAnswerSelect(index)}
+                          className={`quiz-answer ${!showFeedback ? 'hover:bg-amber-600 hover:text-amber-600 hover:bg-opacity-15' : ''} ${
+                            selectedAnswer === index
+                              ? option.isCorrect
+                                ? 'correct'
+                                : 'incorrect'
+                              : ''
+                          } ${flashIndex === index ? 'flash' : ''}`}
+                          disabled={showFeedback}
+                        >
+                          <span className="text-center bg-amber-300 bg-opacity-25 rounded  px-1 mr-1">{answerLabels[index]}</span> {option.text}
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                {showFeedback && !questions[questionIndex].answers[selectedAnswer!].isCorrect && (
+                  <div className="quiz-explanation">
+                    <p>{explanation}</p>
+                  </div>
+                )}
+                {showFeedback && (
+                  <div className="text-center">
+                    <button
+                      onClick={handleNextQuestion}
+                      className={`quiz-next-button ${rammetto.className}`}
+                    >
+                      {questionIndex + 1 >= questions.length ? 'Finalizar Quiz' : 'Próxima Pergunta'}
+                    </button>
+                  </div>
+                )}
 
-        <div className="fixed bottom-8 right-8 flex items-center justify-center">
-          <IconButton name="try-again" size={72} onClick={handleSelect} />
-        </div>
+              </div>
+              <div className="quiz-score-time-container">
+                <div className={`quiz-score ${fontNumbers.className}`}>
+                  <span className=""><Image src="/coin-star.png" alt="Pontos" width={36} height={36} /></span><span>{score}</span>
+                </div>
+                <div className="quiz-time">
+                  <Image src="/clock.png" alt="Tempo" width={36} height={36} />            
+                  <span className={`${fontNumbers.className} text-3xl font-bold min-w-30 mr-1`}>{formatTime(elapsedTime)}</span>
+                </div>
+              </div>   
+            </div>
+            
+          </div>          
 
+            
         </div>
-        <div className="flex justify-between w-full px-56 pb-10">
-          <div className={`${fontNumbers.className} text-3xl text-white font-bold bg-orange-600 px-2 py-2 rounded-3xl min-w-36 flex items-center gap-3`}>
-             <span className=""><Image src="/coin-star.png" alt="Pontos" width={36} height={36} /></span><span>{score}</span>
+        
+        <div className='quiz-sidebar-container'>
+          <div className='quiz-sidebar-content'>
+          <div className="quiz-sidebar-card">
+            <div className="quiz-sidebar-avatar-name">                
+                <div className={`flex rounded-full shadow-lg ${avatarBgColor} border-4 border-orange-700`}>
+                  <Image src={avatarSrc} alt="Avatar" width={70} height={70} className="rounded-full" />
+                </div>
+                <div className={`quiz-sidebar-player-name ${rammetto.className}`}>{playerName}</div>
+            </div>
+
+            <div className="quiz-sidebar-score-time-container">
+                <div className={`quiz-sidebar-score ${fontNumbers.className}`}>
+                  <div className=""><Image src="/coin-star.png" alt="Pontos" width={24} height={24} /></div><div>{score}</div>
+                </div>
+                <div className="quiz-sidebar-time">
+                  <Image src="/clock.png" alt="Tempo" width={24} height={24} />            
+                  <div className={`${fontNumbers.className} font-bold min-w-30`}>{formatTime(elapsedTime)}</div>
+                </div>
+            </div>  
+            <span className="top"></span>
+            <span className="right"></span>
+            <span className="bottom"></span>
+            <span className="left"></span>
           </div>
-          <div className="flex items-center justify-between text-xl text-amber-700 font-bold bg-orange-600 px-2 py-2 rounded-3xl min-w-36 mr-2 gap-1">
-            <Image src="/clock.png" alt="Tempo" width={36} height={36} />            
-            <span className={`${fontNumbers.className} text-3xl font-bold text-white min-w-30 mr-1`}>{formatTime(elapsedTime)}</span>
           </div>
         </div>
       </div>
-    </div>
+      {isPlaying !== null && (
+                <IconButton name={`${isPlaying ? "music-on": "music-off"}`} size={72} onClick={toggle} float='left' />
+        )}
+
+        <IconButton name="try-again" size={72} onClick={handleSelect} float='right' />
+    </>
   );
 }
